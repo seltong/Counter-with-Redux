f8699d1094b1ba689b730edb3967aa212f27b74f
Selton Guedes <sfgs@ecomp.poli.br>
2020-09-23 16:09:49 -0300

    create a basic counter with React and Redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
+import './style/global.css';
+
+function getValueOfElementById(id) {
+  return parseInt(document.getElementById(id).value);
+}
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1>Counter {counter}</h1>
+      </div>
+
+      <hr />
+      
+      <div className="buttons-container">
+        <button onClick={() => dispatch(increment(1))}>+</button>
+        <button onClick={() => dispatch(decrement(1))}>-</button>
+      </div>
+
+      <hr />
+
+      <div className="custom-container">
+        <span>Type a custom value:</span>
+        <input type="number" id='inptCustomValue' />
+        <button onClick={() => dispatch(increment(getValueOfElementById('inptCustomValue')))}>Add</button>
+        <button onClick={() => dispatch(decrement(getValueOfElementById('inptCustomValue')))}>Subtract</button>
+      </div>
+
+      {isLogged ? <h3>Valuable Information I shouldn't see</h3> : ''}
+
+      <footer className="footer">
+        <span>&copy; By Selton Guedes 2020</span>
+      </footer>
     </div>
   );
 }
